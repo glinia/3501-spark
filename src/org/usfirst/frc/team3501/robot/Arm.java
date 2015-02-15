92a1f4064f289b0ccc61739607b09c10e115802c
Logan Howard <logan@oflogan.com>
2015-02-15 01:48:08 -0800

    add PID to arms

--- src/org/usfirst/frc/team3501/robot/Arm.java
+++ src/org/usfirst/frc/team3501/robot/Arm.java
@@ -1,28 +1,48 @@
 package org.usfirst.frc.team3501.robot;
 
 import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDController;
 
 public class Arm extends FireBot {
 
-    private CANJaguar left, right;
-    private Encoder leftShaft, rightShaft;
+    private CANJaguar     leftJ, rightJ;
+    private PIDController left,  right;
 
     private int level;
 
     public Arm() {
-        left  = new CANJaguar(LEFT_WINCH_ADDRESS);
-        right = new CANJaguar(RIGHT_WINCH_ADDRESS);
+        initJags();
 
-        leftShaft  = new Encoder(LEFT_WINCH_ENCODER_A,  LEFT_WINCH_ENCODER_B);
-        rightShaft = new Encoder(RIGHT_WINCH_ENCODER_A, RIGHT_WINCH_ENCODER_B);
+        Encoder leftShaft  = new Encoder(LEFT_WINCH_ENCODER_A,  LEFT_WINCH_ENCODER_B,
+                false, EncodingType.k4X);
+        Encoder rightShaft = new Encoder(RIGHT_WINCH_ENCODER_A, RIGHT_WINCH_ENCODER_B,
+                false, EncodingType.k4X);
 
+        left  = new PIDController(P, I, D, leftShaft, leftJ);
+        right = new PIDController(P, I, D, rightShaft, rightJ);
+        
+        left.enable();
+        right.enable();
+        
         level = START_LEVEL;
+    }
+    
+    private void initJags() {
+        leftJ  = new CANJaguar(LEFT_WINCH_ADDRESS);
+        rightJ = new CANJaguar(RIGHT_WINCH_ADDRESS);
+        
+//        leftJ.setCurrentMode(CANJaguar.kQuadEncoder, PULSES_PER_REV, P, I, D);
+//        rightJ.setCurrentMode(CANJaguar.kQuadEncoder, PULSES_PER_REV, P, I, D);
+//        
+//        leftJ.enableControl();
+//        rightJ.enableControl();
     }
 
     public void set(double speed) {
-        left.set(speed);
-        right.set(speed);
+        left.setSetpoint(speed);
+        right.setSetpoint(speed);
     }
 
     public void setLevel(int level) {
